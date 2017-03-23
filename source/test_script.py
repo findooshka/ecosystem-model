dceac10cefe7c676c92c8e2d1341bd09b71f1188
Arsen_RL <arsensultanovgit@gmail.com>
2017-03-23 16:27:27 +0100

    test script update

--- source/test_script.py
+++ source/test_script.py
@@ -8,7 +8,6 @@ import numpy as np
 
 import source.map
 import source.measurement
-import source.random_forest
 
 
 def sigint_handler(signum, frame):
@@ -46,12 +45,13 @@ def make_one_test(map_size,
                   change_reproduction_threshold=True,
                   change_reproduction_cooldown=True,
                   change_inital_satiation=True,
-                  change_hunt_range=False):
+                  change_hunt_range=True,
+                  change_decease_rate=True,
+                  change_life_duration=True):
     global species
     return_data = []
     iterations_survived = None
     species_count = 0
-    species_count_list = []
     current_species = np.array(species.copy())
     if change_satiation_threshold:
         current_species[:, 7] *= (0.5 + 2 * np.random.rand(current_species.shape[0]))
@@ -63,51 +63,40 @@ def make_one_test(map_size,
         current_species[:, 1] *= (0.5 + 2 * np.random.rand(current_species.shape[0]))
     if change_hunt_range:
         current_species[:, 10] += np.random.randint(-2, 4, current_species.shape[0])
+    if change_decease_rate:
+        for species_index in range(current_species.shape[0]):
+            if len(current_species[species_index][3]) != 0:
+                current_species[species_index][11] *= (0.25 + 3 * np.random.rand())
+                current_species[species_index][12] *= (0.25 + 3 * np.random.rand())
+                current_species[species_index][13] *= (0.25 + 3 * np.random.rand())
+    if change_life_duration:
+        current_species[:, 14] *= (0.5 + 2 * np.random.rand(current_species.shape[0]))
     inital_count = []
     for bounds in inital_beings_number_bounds:
         inital_count.append(np.random.randint(bounds[0], bounds[1] + 1))
         if inital_count[-1] != 0:
             species_count += 1
-    species_count_list += [inital_count]
-    the_map = source.map.Map(inital_count, map_size, map_size, current_species)
-    return_data = np.concatenate(current_species[:, [1, 5, 6, 7, 10]]).tolist()
+    the_map = source.map.Map(inital_count, 60, 60, current_species, ["gaussian", "random", "random"], decease_spread_range=3)
+    return_data = np.concatenate(current_species[:, [1, 5, 6, 7, 10, 11, 12, 13, 14]]).tolist()
     for iteration in range(max_iterations):
         if not source.measurement.survival_simulation(the_map, species_count):
             iterations_survived = iteration
             break
     else:
         iterations_survived = max_iterations
-    print(species_count_list)
-    return species_count_list + return_data + [iterations_survived]
+    return [inital_count + return_data + [iterations_survived]]
 
 
 def main():
     global tests_df
     test_count = 0
     while True:
+        print('Started scripterino')
         test = make_one_test(50,
-                            max_iterations=6000,
+                            max_iterations=10,
                             test_count=10000,
                             inital_beings_number_bounds=[(50, 400), (2, 10), (1000, 6000)],
-                            simulations_per_test=1,
-                            change_satiation_threshold=True,
-                            change_reproduction_threshold=True,
-                            change_reproduction_cooldown=True,
-                            change_inital_satiation=True,
-                            change_hunt_range=True)
-        #test = source.measurement.survival_tests(os.path.join("data", "tests.csv"), # doesn't mean anything, see sigint_handler
-        #                                       species,
-        #                                       50,
-        #                                       max_iterations=6000,
-        #                                       test_count=1,
-        #                                       inital_beings_number_bounds=[(50, 400), (2, 10), (1000, 6000)],
-        #                                       simulations_per_test=1,
-        #                                       change_satiation_threshold=True,
-        #                                       change_reproduction_threshold=True,
-        #                                       change_reproduction_cooldown=True,
-        #                                       change_inital_satiation=True,
-        #                                       change_hunt_range=True,
-        #                                       stop_hour=9)
+                            simulations_per_test=1)
         tests_df = tests_df.append(test, ignore_index=True)
         test_count += 1
         print('done {} tests'.format(test_count))
@@ -116,22 +105,39 @@ def main():
 tests_filename = None
 
 
-species = [(0.05, 130, 0, {2}, (0, 0, 1, 1), 200, 101, 100, 0.01, 0.15, 0),
-           (0.15, 130, 1, {0}, (1, 0, 0, 1), 400, 151, 150, 0.01, 0.15, 3),
-           (0, 10, 2, set(), (0, 0.8, 0.2, 1), 50, 40, 1000, 0.01, 0.15, 0),
+species = [(0.06, 130, 0, {2}, (0, 0, 1, 1), 50, 100, 100, 0.01, 0.15, 0, 0.02, 0.1, 0.2, 150),
+           (0.14, 130, 1, {0}, (1, 0, 0, 1), 100, 151, 150, 0.01, 0.15, 5, 0.06, 0.1, 0.03, 200),
+           (0, 10, 2, set(), (0, 0.8, 0.2, 1), 50, 40, 1000, 0.01, 0.15, 0, 0, 0, 0, 3000),
           ]
 
 
-columns = ['species 0 inital count', 'species 1 inital count',
-       'species 2 inital count', 'species 0 inital satiation',
-       'species 0 satiation threshold', 'species 0 reproduction cooldown',
-       'species 0 reproduction threshold', 'species 0 hunt range',
-       'species 1 inital satiation', 'species 1 satiation threshold',
+columns = [
+       'species 0 inital count', 'species 1 inital count', 'species 2 inital count',
+       'species 0 inital satiation',
+       'species 0 reproduction cooldown',
+       'species 0 reproduction threshold',
+       'species 0 satiation threshold',
+       'species 0 hunt range',
+       'species 0 decease rate',
+       'species 0 death rate',
+       'species 0 recovery rate',
+       'species 1 inital satiation',
        'species 1 reproduction cooldown',
-       'species 1 reproduction threshold', 'species 1 hunt range',
-       'species 2 inital satiation', 'species 2 satiation threshold',
+       'species 1 reproduction threshold',
+       'species 1 satiation threshold',
+       'species 1 hunt range',
+       'species 1 decease rate',
+       'species 1 death rate',
+       'species 1 recovery rate',
+       'species 2 inital satiation',
        'species 2 reproduction cooldown',
-       'species 2 reproduction threshold', 'species 2 hunt range', 'result']
+       'species 2 reproduction threshold',
+       'species 2 satiation threshold',
+       'species 2 hunt range',
+       'species 2 decease rate',
+       'species 2 death rate',
+       'species 2 recovery rate',
+       'result']
 
 
 tests_df = pd.DataFrame(columns=columns)
--- test_script.py
+++ test_script.py
@@ -8,7 +8,6 @@ import numpy as np
 
 import source.map
 import source.measurement
-import source.random_forest
 
 
 def sigint_handler(signum, frame):
@@ -41,17 +40,18 @@ def make_one_test(map_size,
                   max_iterations,
                   test_count,
                   inital_beings_number_bounds,
+                  simulations_per_test=1,
                   change_satiation_threshold=True,
                   change_reproduction_threshold=True,
                   change_reproduction_cooldown=True,
                   change_inital_satiation=True,
-                  change_hunt_range=False):
+                  change_hunt_range=True,
+                  change_decease_rate=False,
+                  change_life_duration=True):
     global species
-    global columns
     return_data = []
     iterations_survived = None
     species_count = 0
-    species_count_list = []
     current_species = np.array(species.copy())
     if change_satiation_threshold:
         current_species[:, 7] *= (0.5 + 2 * np.random.rand(current_species.shape[0]))
@@ -63,13 +63,21 @@ def make_one_test(map_size,
         current_species[:, 1] *= (0.5 + 2 * np.random.rand(current_species.shape[0]))
     if change_hunt_range:
         current_species[:, 10] += np.random.randint(-2, 4, current_species.shape[0])
+    if change_decease_rate:
+        for species_index in range(current_species.shape[0]):
+            if len(current_species[species_index][3]) != 0:
+                current_species[species_index][11] *= (0.25 + 3 * np.random.rand())
+                current_species[species_index][12] *= (0.25 + 3 * np.random.rand())
+                current_species[species_index][13] *= (0.25 + 3 * np.random.rand())
+    if change_life_duration:
+        current_species[:, 14] *= (0.5 + 2 * np.random.rand(current_species.shape[0]))
     inital_count = []
     for bounds in inital_beings_number_bounds:
         inital_count.append(np.random.randint(bounds[0], bounds[1] + 1))
         if inital_count[-1] != 0:
             species_count += 1
-    the_map = source.map.Map(inital_count, map_size, map_size, current_species)
-    return_data = np.concatenate(current_species[:, [1, 5, 6, 7, 10]]).tolist()
+    the_map = source.map.Map(inital_count, 60, 60, current_species, ["gaussian", "random", "random"], decease_spread_range=3)
+    return_data = np.concatenate(current_species[:, [1, 5, 6, 7, 10, 11, 12, 13, 14]]).tolist()
     for iteration in range(max_iterations):
         if not source.measurement.survival_simulation(the_map, species_count):
             iterations_survived = iteration
@@ -82,29 +90,13 @@ def make_one_test(map_size,
 def main():
     global tests_df
     test_count = 0
+    print('Started scripterino')
     while True:
         test = make_one_test(50,
-                            max_iterations=6000,
+                            max_iterations=10,
                             test_count=10000,
                             inital_beings_number_bounds=[(50, 400), (2, 10), (1000, 6000)],
-                            change_satiation_threshold=True,
-                            change_reproduction_threshold=True,
-                            change_reproduction_cooldown=True,
-                            change_inital_satiation=True,
-                            change_hunt_range=True)
-        #test = source.measurement.survival_tests(os.path.join("data", "tests.csv"), # doesn't mean anything, see sigint_handler
-        #                                       species,
-        #                                       50,
-        #                                       max_iterations=6000,
-        #                                       test_count=1,
-        #                                       inital_beings_number_bounds=[(50, 400), (2, 10), (1000, 6000)],
-        #                                       simulations_per_test=1,
-        #                                       change_satiation_threshold=True,
-        #                                       change_reproduction_threshold=True,
-        #                                       change_reproduction_cooldown=True,
-        #                                       change_inital_satiation=True,
-        #                                       change_hunt_range=True,
-        #                                       stop_hour=9)
+                            simulations_per_test=1)
         tests_df = tests_df.append(test, ignore_index=True)
         test_count += 1
         print('done {} tests'.format(test_count))
@@ -113,22 +105,42 @@ def main():
 tests_filename = None
 
 
-species = [(0.05, 130, 0, {2}, (0, 0, 1, 1), 200, 101, 100, 0.01, 0.15, 0),
-           (0.15, 130, 1, {0}, (1, 0, 0, 1), 400, 151, 150, 0.01, 0.15, 3),
-           (0, 10, 2, set(), (0, 0.8, 0.2, 1), 50, 40, 1000, 0.01, 0.15, 0),
+species = [(0.06, 130, 0, {2}, (0, 0, 1, 1), 50, 100, 100, 0.01, 0.15, 0, 0.02, 0.1, 0.2, 150),
+           (0.14, 130, 1, {0}, (1, 0, 0, 1), 100, 151, 150, 0.01, 0.15, 5, 0.06, 0.1, 0.03, 200),
+           (0, 10, 2, set(), (0, 0.8, 0.2, 1), 50, 40, 1000, 0.01, 0.15, 0, 0, 0, 0, 3000),
           ]
 
 
-columns = ['species 0 inital count', 'species 1 inital count',
-       'species 2 inital count', 'species 0 inital satiation',
-       'species 0 satiation threshold', 'species 0 reproduction cooldown',
-       'species 0 reproduction threshold', 'species 0 hunt range',
-       'species 1 inital satiation', 'species 1 satiation threshold',
+columns = [
+       'species 0 inital count', 'species 1 inital count', 'species 2 inital count',
+       'species 0 inital satiation',
+       'species 0 reproduction cooldown',
+       'species 0 reproduction threshold',
+       'species 0 satiation threshold',
+       'species 0 hunt range',
+       'species 0 decease rate',
+       'species 0 death rate',
+       'species 0 recovery rate',
+       'species 0 life duration',
+       'species 1 inital satiation',
        'species 1 reproduction cooldown',
-       'species 1 reproduction threshold', 'species 1 hunt range',
-       'species 2 inital satiation', 'species 2 satiation threshold',
+       'species 1 reproduction threshold',
+       'species 1 satiation threshold',
+       'species 1 hunt range',
+       'species 1 decease rate',
+       'species 1 death rate',
+       'species 1 recovery rate',
+       'species 1 life duration',
+       'species 2 inital satiation',
        'species 2 reproduction cooldown',
-       'species 2 reproduction threshold', 'species 2 hunt range', 'result']
+       'species 2 reproduction threshold',
+       'species 2 satiation threshold',
+       'species 2 hunt range',
+       'species 2 decease rate',
+       'species 2 death rate',
+       'species 2 recovery rate',
+       'species 2 life duration',
+       'result']
 
 
 tests_df = pd.DataFrame(columns=columns)
